f8830d8e715f27a8dc4d1a0a131faa07dd55ea84
JSQ47 <Jsalazarq47@outlook.com>
2025-07-23 11:56:04 -0500

    App pruebas

--- app/services/usuario_service.py
+++ app/services/usuario_service.py
@@ -1,10 +1,16 @@
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError  # <- importante
+from fastapi import HTTPException
 from passlib.hash import bcrypt
 from app.repositories.usuario_repo import create_user, get_user_by_email
 
 def register_user(db: Session, nombre: str, email: str, password: str):
     hashed_password = bcrypt.hash(password)
-    return create_user(db, nombre, email, hashed_password)
+    try:
+        return create_user(db, nombre, email, hashed_password)
+    except IntegrityError:
+        db.rollback()  # <- MUY importante o tu sesión se bloquea
+        raise HTTPException(status_code=409, detail="El email ya está registrado.")
 
 def authenticate_user(db: Session, email: str, password: str):
     user = get_user_by_email(db, email)
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\jsala\Desktop\PROGRAMACION\trivago\venv
+set VIRTUAL_ENV=C:\Users\jsala\Desktop\PROGRAMACION\HotelAPI\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
